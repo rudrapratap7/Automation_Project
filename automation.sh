6123b6ae07d739bd044d6a288b56019bc6eaf388
root <root@ip-172-31-86-139.ec2.internal>
2022-07-07 16:21:12 +0000

    Automation-vo.1

---
+++ automation.sh
@@ -0,0 +1,29 @@
+#!/bin/bash                                                                                                                                                                                   
+# Variables                                                                                    
+name="abhishek"                                                                                
+s3_bucket="upgrad-abhishek"                                                                                                                                                                   
+apt update -y                                                                                                                                                                                
+if [[ apache2 != $(dpkg --get-selections apache2 | awk '{print $1}') ]]; then                           
+apt install apache2 -y                                                               
+fi                                                                                                                                                                                           
+running=$(systemctl status apache2 | grep active | awk '{print $3}' | tr -d '()')             
+if [[ running != ${running} ]]; then                                                                                                                                                                                                                                                                 
+systemctl start apache2                                                                
+fi                                                                                                                                                                                            
+enabled=$(systemctl is-enabled apache2 | grep "enabled")                                                                                                                                      
+if  [[ enabled != ${enabled} ]]; then                                                                    
+systemctl enable apache2                                                             
+fi                                                                                                                                                                                            
+timestamp=$(date '+%d%m%Y-%H%M%S')                                                             
+cd /var/log/apache2                                                                            
+tar -cf  /tmp/${abhishek}-httpd-logs-${timestamp}.tar *.log                                                                                                                                   
+if [[ -f /tmp/${abhishek}-httpd-logs-${timestamp}.tar ]]; then                                          
+aws s3 cp /tmp/${abhishek}-httpd-logs-${timestamp}.tar s3://${upgrad-abhishek}/${abhishek}-httpd-logs-${timestamp}.tar                                                                                                                                                              
+fi   
+
+
+
+
+
+   
+
