a7e4f301799b3b8943fac339801e809bdd82e31d
root <root@ip-172-31-86-139.ec2.internal>
2022-07-07 16:51:17 +0000

    Automation-vo.2

--- automation.sh
+++ automation.sh
@@ -21,6 +21,23 @@ if [[ -f /tmp/${abhishek}-httpd-logs-${timestamp}.tar ]]; then
 aws s3 cp /tmp/${abhishek}-httpd-logs-${timestamp}.tar s3://${upgrad-abhishek}/${abhishek}-httpd-logs-${timestamp}.tar                                                                                                                                                              
 fi   
 
+docroot="/var/www/html"
+if [[ ! -f ${docroot}/inventory.html ]]; then
+echo -e 'Log Type\t-\tTime Created\t-\tTypeSize' > ${docroot}/inventory.html
+fi
+
+if [[ -f ${docroot}/inventory.html ]]; then
+echo -e 'Log Type\t-\tTime Created\t-\tType\t-\tSize' > ${docroot}/inventory.html
+fi
+
+if [[ -f ${docroot}/inventory.html ]]; then
+size=$(du -h /tmp/${name}-httpd-logs-${timestamp}.tar | awk '{print $1}')
+echo -e "httpd-logs\t-\t${timestamp}\t-\ttar\t-\t${size}" >> ${docroot}/inventory.html
+fi
+
+if [[ ! -f /etc/cron.d/automation ]]; then
+echo "* * * * * root /root/automation.sh" >> /etc/cron.d/automation
+fi
 
 
 
